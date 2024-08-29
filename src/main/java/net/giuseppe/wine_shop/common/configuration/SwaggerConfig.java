3516ae62f7a3689baf3ba55d8169b513961790bf
GabrielGiuseppe <47945875+GabrielGiuseppe@users.noreply.github.com>
2024-08-29 18:10:57 -0300

    Swagger

---
+++ src/main/java/net/giuseppe/wine_shop/common/configuration/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package net.giuseppe.wine_shop.common.configuration;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
